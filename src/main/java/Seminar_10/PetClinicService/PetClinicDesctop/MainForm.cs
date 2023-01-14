0828b342e311d73820ec3699ae9bf59d013fa368
Dmitriy Lobanov <dmitriyylobanov2@gmail.com>
2023-01-14 14:44:11 +0300

    Подклчение desctop  к веб сервису // успешно

---
+++ src/main/java/Seminar_10/PetClinicService/PetClinicDesctop/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using PetClinicServiceNamespace;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetClinicDesctop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadClients_Click(object sender, EventArgs e)
+        {
+            PetClinicServiceClient petClinicServiceClient = new PetClinicServiceClient("http://localhost:5083", new System.Net.Http.HttpClient());
+            ICollection<Client> clients = petClinicServiceClient.GetAllAllAsync().Result;
+            
+            listViewClients.Items.Clear();
+            foreach (Client client in clients)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = client.ClientId.ToString();
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                { 
+                    Text = client.SurName                
+                });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = client.FirstName
+                });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = client.Patronymic
+                });
+                listViewClients.Items.Add(item);
+            }
+        }
+    }
+}
